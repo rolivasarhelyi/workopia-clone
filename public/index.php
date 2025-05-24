80c184e495e9c16f6a8e2f83dc1328cecfefa7d1
Roland Vásárhelyi <roli.vasarhelyi@gmail.com>
2025-05-24 11:01:20 +0200

    Added PSR-4 Autoloader

--- public/index.php
+++ public/index.php
@@ -1,12 +1,13 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
 
-spl_autoload_register(function ($class) {
-    $path = basePath('Framework/' . $class . '.php');
-    if (file_exists($path)) {
-        require $path;
-    }
-});
+// spl_autoload_register(function ($class) {
+//     $path = basePath('Framework/' . $class . '.php');
+//     if (file_exists($path)) {
+//         require $path;
+//     }
+// });
 
 // Instantiate the router 
 $router = new Router();
