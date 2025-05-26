4916d364be7133abed741589d96989de6944fb7a
Roland Vásárhelyi <roli.vasarhelyi@gmail.com>
2025-05-26 15:44:20 +0200

    Implemented delete listings functionality

--- routes.php
+++ routes.php
@@ -6,3 +6,5 @@ $router->get('/listings/create', 'ListingController@create');
 $router->get('/listings/{id}', 'ListingController@show');
 
 $router->post('/listings', 'ListingController@store');
+
+$router->delete('/listings/{id}', 'ListingController@destroy');
